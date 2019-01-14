a68f056939e58324e242c47242ff322251b7a0cc
Gary Bermudez and Randee Layosa <randee@>
2019-01-14 13:52:31 -0800

    add link to the script file and README file

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+var add = function(number1, number2){
+return number1 + number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+
+alert(add(number1, number2));
+
+var substract = function(number1, number2){
+return number1 - number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+
+alert(substract(number1, number2));
+
+var multiplication = function(number1, number2){
+return number1 * number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+
+alert (multiplication(number1, number2));
+
+var division = function(number1, number2){
+return number1 / number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+
+alert (division(number1, number2));
