5e562d36946d8e825f5fc93f8edc7cf5b79dacc5
Gary Bermudez and Randee Layosa <randee@>
2019-01-14 14:53:31 -0800

    Add basic arithmetic functions, and BMI calculator.

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,38 @@
-var add = function(number1, number2){
-return number1 + number2;
+var add = function(number1, number2) {
+  return number1 + number2;
 };
 
 var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-
+var number2 = parseInt(prompt("Enter another number to add:"));
+var result = add(number1, number2);
 alert(add(number1, number2));
 
-var substract = function(number1, number2){
-return number1 - number2;
+
+var subtract = function(number1, number2){
+  return number1 - number2;
 };
 
 var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+var number2 = parseInt(prompt("Enter another number to subtract:"));
+var result = subtract(number1, number2);
+alert(subtract(number1, number2));
 
-alert(substract(number1, number2));
 
-var multiplication = function(number1, number2){
-return number1 * number2;
+var multiply = function(number1, number2){
+  return number1 * number2;
 };
 
 var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+var number2 = parseInt(prompt("Enter another number to multiply:"));
+var result = multiply(number1, number2);
+alert(multiply(number1, number2));
 
-alert (multiplication(number1, number2));
 
-var division = function(number1, number2){
-return number1 / number2;
+var divide = function(number1, number2){
+  return number1 / number2;
 };
 
 var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-
-alert (division(number1, number2));
+var number2 = parseInt(prompt("Enter another number to divide:"));
+var result = divide(number1, number2);
+alert (divide(number1, number2));
