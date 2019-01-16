5407cf3642a77c30e3dfabce2ab63bbfbbdeac0a
Nathan Nielson and Randee Layosa <randee@>
2019-01-16 13:30:27 -0800

    Add correct functionality for business and interface logic.

--- js/scripts.js
+++ js/scripts.js
@@ -2,46 +2,60 @@ var add = function(number1, number2){
   return number1 + number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number to add:"));
-var result = add(number1, number2);
-alert(add(number1, number2));
-
-
 var subtract = function(number1, number2){
   return number1 - number2;
 };
-
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number to subtract:"));
-var result = subtract(number1, number2);
-alert(subtract(number1, number2));
-
 
 var multiply = function(number1, number2){
   return number1 * number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number to multiply:"));
-var result = multiply(number1, number2);
-alert(multiply(number1, number2));
-
-
 var divide = function(number1, number2){
   return number1 / number2;
 };
-
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number to divide:"));
-var result = divide(number1, number2);
-alert (divide(number1, number2));
 
 
 var bmi = function(weight, height){
   return weight / (height * height) * parseInt("703");
 };
 
-var weight = parseInt(prompt("Enter your weight in pounds:"));
-var height = parseInt(prompt("Enter your height in inches:"));
-alert(bmi(weight,height));
+// var weight = parseInt(prompt("Enter your weight in pounds:"));
+// var height = parseInt(prompt("Enter your height in inches:"));
+// alert(bmi(weight,height));
+//
+
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#subtract1").val());
+    var number2 = parseInt($("#subtract2").val());
+    var result = subtract(number1, number2);
+    $("#output").text(result);
+  });
+
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+
+});
